96daee4002cadbf57abf089bc2f867f21cb80f03
Matheus <matheuscontato96@hotmail.com>
2022-12-28 12:42:07 -0300

    testando

---
+++ script.js
@@ -0,0 +1,14 @@
+const p = document.getElementById('contador');
+const btn = document.getElementById('btn');
+const reset = document.getElementById('reset');
+let cont = 0;
+
+p.innerHTML = cont;
+
+btn.addEventListener('click',function contar(){
+  p.innerHTML = ++cont;
+});
+reset.addEventListener('click',function resetar(){
+      p.innerHTML = 0;
+      cont = 0;
+})
